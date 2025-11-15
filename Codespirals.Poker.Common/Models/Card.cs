f3cdae79553382fe04de109f551bec7614c368ff
Eli <elias.styner@gmail.com>
2025-11-16 00:27:33 +0100

    built out almost all of the blackjack game. Only dealer drawing multiple times after you stay is missing.

--- Codespirals.Poker.Common/Models/Card.cs
+++ Codespirals.Poker.Common/Models/Card.cs
@@ -2,24 +2,27 @@
 
 public class Card
 {
+    private readonly string _emoji = "?";
     public byte CardCode { get; init; }
     public byte Value { get; init; }
     public string Name { get; init; }
     public Suit Suit { get; init; }
-    public string? Emoji { get; init; }
+    public bool IsFaceDown { get; set; }
+    public string? Emoji => IsFaceDown ? Constants.CARDBACKEMOJI : _emoji;
 
-    public Card(): this(byte.MinValue)
+    public Card(bool faceDown = false): this(byte.MinValue, faceDown)
     {
 
     }
 
-    public Card(byte cardCode)
+    public Card(byte cardCode, bool faceDown = false)
     {
         CardCode = cardCode;
         // bits 4-7 are suit
         Suit = (Suit)(CardCode >> 4);
         // bits 0-3 are value
         Value = (byte)(CardCode & 0b_0000_1111);
+        IsFaceDown = faceDown;
 
         if (Value is byte.MinValue)
         {
@@ -29,20 +32,20 @@ public class Card
         else if (Value is (byte)SpecialCardNames.Joker)
         {
             Name = SpecialCardNames.Joker.ToString();
-            Emoji = Constants.JOKEREMOJI;
+            _emoji = Constants.JOKEREMOJI;
         }
         // fool
         else if (Value is (byte)SpecialCardNames.Fool)
         {
             Name = SpecialCardNames.Fool.ToString();
-            Emoji = Constants.FOOLEMOJI;
+            _emoji = Constants.FOOLEMOJI;
         }
         else
         {
             Name = Value is not 1 and < 11 ? Value.ToString() : ((SpecialCardNames)Value).ToString();
             // if the card is of traditinal suits, there's an emoji for it
             if ((byte)Suit is > 0 and < 5)
-                Emoji = char.ConvertFromUtf32(Convert.ToInt32("01F0A0", 16) + Value + (((int)Suit) * 16));
+                _emoji = char.ConvertFromUtf32(Convert.ToInt32("01F0A0", 16) + Value + (((int)Suit-1) * 16));
         }
     }
 
---
+++ Codespirals.Poker.Common/Models/Hand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Codespirals.Poker;
+public class Hand
+{
+    private List<Card> _cards = [];
+    public ReadOnlyCollection<Card> Cards => _cards.AsReadOnly();
+
+    public void Add(Card card)
+        => _cards.Add(card);
+    public void Add(IEnumerable<Card> card)
+        => _cards.AddRange(card);
+
+    public Card? Take(byte cardCode)
+        => _cards.FirstOrDefault(c => c.CardCode == cardCode);
+    public IEnumerable<Card> Take(byte[] cardCodes)
+        => cardCodes.Select(code => _cards.FirstOrDefault(c => c.CardCode == code)).WhereNotNull();
+}
